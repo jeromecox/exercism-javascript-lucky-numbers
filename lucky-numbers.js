977b6e18b5bea2e9be551ad4f2bab784a918be46
jeromecox <jeromecox@hotmail.com>
2022-12-23 14:04:40 -0600

    solved all

--- lucky-numbers.js
+++ lucky-numbers.js
@@ -30,5 +30,9 @@ export function luckyNumber(value) {
  * @returns {string} error message
  */
 export function errorMessage(input) {
-  throw new Error("Implement the errorMessage function");
+  if (!input) {
+    return "Required field";
+  } else if (!Number(input) || input == 0) {
+    return "Must be a number besides 0";
+  } else return "";
 }
